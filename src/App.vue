4e65e448e7049b243cde8b9bafacd4a89b088f57
weixing <245783065@qq.com>
2022-01-13 10:14:29 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex items-center justify-center align-middle">
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="rounded-lg md:w-56"
+              src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+              alt="Woman paying for a purchase"
+            />
+          </div>
+          <div class="mt-4 md:mt-0 md:ml-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+            >Finding customers for your new business</a>
+            <p
+              class="mt-2 text-gray-600"
+            >Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
